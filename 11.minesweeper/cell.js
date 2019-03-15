a5903f68d1d7623c23214944b86f7a1a81ae9a52
Space <webproxa@gmail.com>
2019-03-15 23:24:37 +0300

    adding basic functionality

--- 11.minesweeper/cell.js
+++ 11.minesweeper/cell.js
@@ -4,22 +4,24 @@ class Cell {
 		this.y = y;
 		this.n = n;
 		this.state = null;
-		this.stateN = null;
+		this.show = false;
 	}
 
 
-	show() {
+	draw() {
+		if (!this.show) return;
+
 		if (this.state === -1) {
 			fill(0);
 			rect(this.x, this.y, res, res);
 			fill(255, 0, 0);
-			text(this.state, this.x + res / 3, this.y + res / 2);
+			text(this.state, this.x + res / 2, this.y + res / 2);
 		} else if (this.state > 0) {
 			fill(0, 255, 0);
-			text(this.state, this.x + res / 3, this.y + res / 2);
+			text(this.state, this.x + res / 2, this.y + res / 2);
 		} else {
 			fill(0, 0, 255);
-			text(this.state, this.x + res / 3, this.y + res / 2);
+			text(this.state, this.x + res / 2, this.y + res / 2);
 		}
 
 		// fill(100);
--- 11.minesweeper/sketch.js
+++ 11.minesweeper/sketch.js
@@ -1,22 +1,47 @@
+let raw = [{"x":0,"y":0,"n":0,"state":0,"show":false},{"x":64,"y":0,"n":1,"state":0,"show":false},{"x":128,"y":0,"n":2,"state":0,"show":false},{"x":192,"y":0,"n":3,"state":0,"show":false},{"x":256,"y":0,"n":4,"state":1,"show":false},{"x":320,"y":0,"n":5,"state":-1,"show":false},{"x":384,"y":0,"n":6,"state":1,"show":false},{"x":448,"y":0,"n":7,"state":0,"show":false},{"x":512,"y":0,"n":8,"state":1,"show":false},{"x":576,"y":0,"n":9,"state":-1,"show":false},{"x":0,"y":64,"n":10,"state":0,"show":false},{"x":64,"y":64,"n":11,"state":0,"show":false},{"x":128,"y":64,"n":12,"state":0,"show":false},{"x":192,"y":64,"n":13,"state":0,"show":false},{"x":256,"y":64,"n":14,"state":1,"show":false},{"x":320,"y":64,"n":15,"state":1,"show":false},{"x":384,"y":64,"n":16,"state":1,"show":false},{"x":448,"y":64,"n":17,"state":0,"show":false},{"x":512,"y":64,"n":18,"state":1,"show":false},{"x":576,"y":64,"n":19,"state":1,"show":false},{"x":0,"y":128,"n":20,"state":0,"show":false},{"x":64,"y":128,"n":21,"state":0,"show":false},{"x":128,"y":128,"n":22,"state":0,"show":false},{"x":192,"y":128,"n":23,"state":0,"show":false},{"x":256,"y":128,"n":24,"state":1,"show":false},{"x":320,"y":128,"n":25,"state":1,"show":false},{"x":384,"y":128,"n":26,"state":1,"show":false},{"x":448,"y":128,"n":27,"state":0,"show":false},{"x":512,"y":128,"n":28,"state":0,"show":false},{"x":576,"y":128,"n":29,"state":0,"show":false},{"x":0,"y":192,"n":30,"state":1,"show":false},{"x":64,"y":192,"n":31,"state":1,"show":false},{"x":128,"y":192,"n":32,"state":1,"show":false},{"x":192,"y":192,"n":33,"state":0,"show":false},{"x":256,"y":192,"n":34,"state":1,"show":false},{"x":320,"y":192,"n":35,"state":-1,"show":false},{"x":384,"y":192,"n":36,"state":1,"show":false},{"x":448,"y":192,"n":37,"state":0,"show":false},{"x":512,"y":192,"n":38,"state":0,"show":false},{"x":576,"y":192,"n":39,"state":0,"show":false},{"x":0,"y":256,"n":40,"state":2,"show":false},{"x":64,"y":256,"n":41,"state":-1,"show":false},{"x":128,"y":256,"n":42,"state":2,"show":false},{"x":192,"y":256,"n":43,"state":0,"show":false},{"x":256,"y":256,"n":44,"state":1,"show":false},{"x":320,"y":256,"n":45,"state":1,"show":false},{"x":384,"y":256,"n":46,"state":1,"show":false},{"x":448,"y":256,"n":47,"state":0,"show":false},{"x":512,"y":256,"n":48,"state":0,"show":false},{"x":576,"y":256,"n":49,"state":0,"show":false},{"x":0,"y":320,"n":50,"state":3,"show":false},{"x":64,"y":320,"n":51,"state":-1,"show":false},{"x":128,"y":320,"n":52,"state":2,"show":false},{"x":192,"y":320,"n":53,"state":0,"show":false},{"x":256,"y":320,"n":54,"state":0,"show":false},{"x":320,"y":320,"n":55,"state":0,"show":false},{"x":384,"y":320,"n":56,"state":0,"show":false},{"x":448,"y":320,"n":57,"state":0,"show":false},{"x":512,"y":320,"n":58,"state":0,"show":false},{"x":576,"y":320,"n":59,"state":0,"show":false},{"x":0,"y":384,"n":60,"state":-1,"show":false},{"x":64,"y":384,"n":61,"state":3,"show":false},{"x":128,"y":384,"n":62,"state":3,"show":false},{"x":192,"y":384,"n":63,"state":2,"show":false},{"x":256,"y":384,"n":64,"state":1,"show":false},{"x":320,"y":384,"n":65,"state":1,"show":false},{"x":384,"y":384,"n":66,"state":1,"show":false},{"x":448,"y":384,"n":67,"state":1,"show":false},{"x":512,"y":384,"n":68,"state":0,"show":false},{"x":576,"y":384,"n":69,"state":0,"show":false},{"x":0,"y":448,"n":70,"state":1,"show":false},{"x":64,"y":448,"n":71,"state":3,"show":false},{"x":128,"y":448,"n":72,"state":-1,"show":false},{"x":192,"y":448,"n":73,"state":-1,"show":false},{"x":256,"y":448,"n":74,"state":1,"show":false},{"x":320,"y":448,"n":75,"state":1,"show":false},{"x":384,"y":448,"n":76,"state":-1,"show":false},{"x":448,"y":448,"n":77,"state":1,"show":false},{"x":512,"y":448,"n":78,"state":0,"show":false},{"x":576,"y":448,"n":79,"state":0,"show":false},{"x":0,"y":512,"n":80,"state":0,"show":false},{"x":64,"y":512,"n":81,"state":2,"show":false},{"x":128,"y":512,"n":82,"state":-1,"show":false},{"x":192,"y":512,"n":83,"state":3,"show":false},{"x":256,"y":512,"n":84,"state":1,"show":false},{"x":320,"y":512,"n":85,"state":1,"show":false},{"x":384,"y":512,"n":86,"state":1,"show":false},{"x":448,"y":512,"n":87,"state":1,"show":false},{"x":512,"y":512,"n":88,"state":0,"show":false},{"x":576,"y":512,"n":89,"state":0,"show":false},{"x":0,"y":576,"n":90,"state":0,"show":false},{"x":64,"y":576,"n":91,"state":1,"show":false},{"x":128,"y":576,"n":92,"state":1,"show":false},{"x":192,"y":576,"n":93,"state":1,"show":false},{"x":256,"y":576,"n":94,"state":0,"show":false},{"x":320,"y":576,"n":95,"state":0,"show":false},{"x":384,"y":576,"n":96,"state":0,"show":false},{"x":448,"y":576,"n":97,"state":0,"show":false},{"x":512,"y":576,"n":98,"state":0,"show":false},{"x":576,"y":576,"n":99,"state":0,"show":false}];
+
+
+
+function parseRaw() {
+	for (let i = 0; i < raw.length; i++) {
+		let r = raw[i];
+		let c = cells.push(new Cell(r.x, r.y, r.n));
+		cells[c-1].state = r.state;
+		cells[c-1].show = r.show;
+	}
+
+	console.log('done');
+}
+
+
+
+
 let rows = 10;
 let res = 64;
+let bombsCount = 1;
 let cells = [];
+let gameEnded = false;
+
+let lines = [];
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
+	// parseRaw();
 	setCells();
 	setBombs();
 	setStates();
 
 	textSize(32);
+	textAlign(CENTER, CENTER);
 }
 
 
 
 function draw() {
 	stroke(100);
+	strokeWeight(1);
 	noFill();
 
 	for (let i = 0; i < rows; i++) {
@@ -27,11 +52,141 @@ function draw() {
 
 	for (let j = 0; j < rows; j++) {
 		for (let i = 0; i < rows; i++) {
-			cells[j * rows + i].show();
+			cells[j * rows + i].draw();
 		}
+	}
+
+
+	// drawLines();
+}
+
+
+
+function mousePressed() {
+	if (gameEnded) return;
+
+	let x = floor(mouseX / res);
+	let y = floor(mouseY / res);
+
+	let idx = y * rows + x;
+	click(idx, x, y);
+}
+
+
+
+function click(idx, x, y) {
+	if (cells[idx].state === -1) endGame(); 
+	else showCells(idx, x, y); 
+}
+
+
+
+
+
+function showCells(idx, x, y) {
+	showCell(idx);
+
+	if (cells[idx].state > 0) return;
+
+
+	let nx, ny;
+
+	nx = x;
+	ny = y - 1;
+
+	if (0 <= nx && nx < rows && 0 <= ny && ny < rows) {
+		let id = ny * rows + nx;
+
+		if ((id >= 0 && id < rows**2) 
+			&& cells[id].state !== -1
+			&& !cells[id].show) showCells(id, nx, ny);
+	}
+
+
+
+	nx = x;
+	ny = y + 1;
+
+	if (0 <= nx && nx < rows && 0 <= ny && ny < rows) {
+		let id = ny * rows + nx;
+
+		if ((id >= 0 && id < rows**2) 
+			&& cells[id].state !== -1
+			&& !cells[id].show) showCells(id, nx, ny);
+	}
+
+
+
+	nx = x - 1;
+	ny = y;
+
+	if (0 <= nx && nx < rows && 0 <= ny && ny < rows) {
+		let id = ny * rows + nx;
+
+		if ((id >= 0 && id < rows**2) 
+			&& cells[id].state !== -1
+			&& !cells[id].show) showCells(id, nx, ny);
+	}
+
+
+
+	nx = x + 1;
+	ny = y;
+
+	if (0 <= nx && nx < rows && 0 <= ny && ny < rows) {
+		let id = ny * rows + nx;
+
+		if ((id >= 0 && id < rows**2) 
+			&& cells[id].state !== -1
+			&& !cells[id].show) showCells(id, nx, ny);
 	}
 }
 
+
+
+
+// function showCells1(idx, x, y, from=null) {
+// 	showCell(idx);
+
+// 	if (from){
+// 		lines.push({
+// 			from: {
+// 				x: from.x * res + (res / 2),
+// 				y: from.y * res + (res / 2)
+// 			},
+// 			to: {
+// 				x: x * res + (res / 2),
+// 				y: y * res + (res / 2)
+// 			}
+// 		});
+// 	}
+// }
+
+
+
+// function drawLines() {
+// 	strokeWeight(2);
+// 	stroke(255, 0, 0);
+
+// 	for (let l of lines) {
+// 		line(l.from.x, l.from.y, l.to.x, l.to.y);
+
+// 		let offset = 10;
+// 		push();
+// 		let angle = atan2(l.from.y - l.to.y, l.from.x - l.to.x); //gets the angle of the line
+// 		translate(l.to.x, l.to.y); //translates to the destination vertex
+// 		rotate(angle-HALF_PI); //rotates the arrow point
+// 		triangle(-offset*0.5, offset, offset*0.5, offset, 0, -offset/2); //draws the arrow point as a triangle
+// 		pop();
+// 	}
+// }
+
+
+
+
+function showCell(idx) {
+	cells[idx].show = true;
+}
 
 
 
@@ -46,7 +201,7 @@ function setCells() {
 
 
 function setBombs() {
-	for (let i = 0; i < rows * 2; i++) {
+	for (let i = 0; i < bombsCount; i++) {
 		let x = floor(random(rows));
 		let y = floor(random(rows));
 		let idx = y * rows + x;
@@ -98,5 +253,17 @@ function getNeighbours(x, y) {
 	}
 
 	return n;
+}
+
+
+
+function endGame() {
+	gameEnded = true;
+
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < rows; i++) {
+			cells[j * rows + i].show = true;
+		}
+	}
 }
 
